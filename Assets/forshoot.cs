ed3a052ca8f97db8e3f7d8b0ce4c0fc9fc62d8f0
Akash Lobo <skywolfdev@gmail.com>
2016-08-04 13:06:37 +0530

    commit 1

---
+++ Assets/forshoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class forshoot : MonoBehaviour {
+	public Vector3 direction = Vector3.forward;
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.rotation * (direction.normalized  * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/move.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * speed;
+	}
+}
